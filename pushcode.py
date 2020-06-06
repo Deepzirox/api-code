19c99b84f9b035205a25e5dcfcd6449a9408d860
Camilo tobon <1611@holbertonschool.com>
2020-06-06 02:42:13 -0500

    fix argv pushcode

--- pushcode.py
+++ pushcode.py
@@ -2,14 +2,14 @@ import requests
 import json
 from sys import argv
 
-if len(argv) != 2:
+if len(argv) != 3:
     raise Exception("Invalid arguments")
 
 def parse_code():
     code = ""
-    numbers = json.loads(requests.get("http://dfc0fa40be8a.ngrok.io/").text)['numbers']
+    numbers = json.loads(requests.get(argv[1]).text)['numbers']
     numbers = int(numbers) + 1
-    with open(argv[1], 'r') as file:
+    with open(argv[2], 'r') as file:
         code = file.read()
     update = {
     f"Code{numbers}" : code,
